23be7dd3b36982ba934dc645811878e99ed97590
Adam Harries <Harries.adam@googlemail.com>
2011-11-10 17:09:16 +0000

    Initial Commit - Added original (old) linked list files, along with node object.

---
+++ Linked List/Original_code/Linked_Node_List/Linked_Node_List.h
@@ -0,0 +1,151 @@
+#ifndef LINKED_NODE_LIST_H//is node already defined, avoids cyclic defines
+#define LINKED_NODE_LIST_H
+
+#include "../Node/Node.h"
+
+class Linked_Node_List
+{
+    public:
+        Linked_Node_List() {/*constructor*/}
+        virtual ~Linked_Node_List() {/*destructor*/}
+        //Add a node to the end of the list
+        void Push_node(Node* dummy)
+        {
+			//if the start node is nothing (ie, the linked list is empty
+            if(LL_START_NODE==NULL)
+            {
+				//make the new node the start node and the end node
+                LL_START_NODE = dummy;
+                LL_END_NODE = dummy;
+            }else{
+				//start node not empty, add the node normally
+				//set the end node to point to the new node
+                LL_END_NODE->SetNext(dummy);
+				//set the new node to point back to the (old) end node
+                dummy->SetPrevious(LL_END_NODE);
+				//set the new end node as the new node
+                LL_END_NODE = dummy;
+            }
+        }
+        //Remove a node from the end of the list, and return the result
+        Node* Pop_node()
+        {
+            Node* dummy;
+            dummy = LL_END_NODE;
+			//if the end node is not nothing (ie, the list is not empty)
+            if(dummy!=NULL)
+            {
+				//if the end node has no previous, ie, the list has only one node in it
+                if(LL_END_NODE->GetPrevious()==NULL)
+                {
+					//set the end and start node to null
+                    LL_END_NODE = NULL;
+                    LL_START_NODE = NULL;
+                }else{//the node has >1 nodes
+					//new end node is the node before previous
+                    LL_END_NODE = LL_END_NODE->GetPrevious();
+					//break link to old end node
+                    LL_END_NODE->SetNext(NULL);
+                }
+            }
+			//return the last node
+            return dummy;
+        }
+		//removes a node from the list, similar to popping, but takes a node* argument to specify a specific node
+        void Remove_node(Node* dummy)
+        {
+			//gets the previous and next nodes to the passed node
+            Node* dummy_prev;
+			Node* dummy_next;
+            dummy_prev = dummy->GetPrevious();
+            dummy_next = dummy->GetNext();
+			//checks to see if they are null
+            if(dummy_prev != NULL)
+				//sets the previous node to point to the next
+                dummy_prev->SetNext(dummy_next);
+            if(dummy_next != NULL)
+				//sets the next node to point to the previous
+                dummy_next->SetNext(dummy_prev);
+        }
+		//iterates through the node and prints information about them
+        void Iterate_nodes()
+        {
+            std::cout<<"Iterating Nodes:"<<"\n";
+            Node* dummy = LL_START_NODE;
+            while(dummy!=NULL)
+            {
+                //print out the data from the node, or draw it or whatever.
+                //std::cout<<"X:"<<dummy->GetX()<<" Y:"<<dummy->GetY()<<"UID:"<<dummy->uid<<"\n";
+                std::cout<<"Node "<<dummy->s_udata<<" with v = "<<dummy->GetValency()<<"\n";
+                dummy = dummy->GetNext();
+            }
+            std::cout<<"Done iterating\n";
+        }
+		//gets a specific node from the list, based upon its position in the list, with error checking to return null nodes if given a number outside the list
+        Node* GetNode(int i)
+		{
+			Node* dummy = LL_START_NODE;
+			//iterates through with a for loop
+			for(int j = 0;j<i;j++)
+			{
+				dummy=dummy->GetNext();
+				//if the node is null, return it, as the loop has gone on beyond the limits of the linked list
+				if(dummy==NULL)
+					return dummy;
+			}
+			return dummy;
+		}
+		//Gets a specific node based upon the userdata of the node
+		Node* StringGetNode(std::string s)
+		{
+		    Node* dummy = LL_START_NODE;
+			//iterates through
+		    while(dummy!=NULL)
+		    {
+				//if the userdata of the iteration node matches the string s
+                if(dummy->s_udata == s)
+                {
+					//return the node*
+                    return dummy;
+                }
+		        dummy = dummy->GetNext();
+		    }
+			//node not found, return null
+		    return NULL;
+		}
+		//clears the list of nodes, by repeatedly popping nodes from the end of the list, and deleting them
+		void ClearList()
+		{
+		    Node* dummy = Pop_node();
+		    while(dummy!=NULL)
+		    {
+		        delete dummy;
+		        dummy = Pop_node();
+		    }
+		}
+		//Class to iterate through the nodes in the linked list, and call a programmed defined function
+		//Passes the function the iteration number, as well as the node.
+		//Also (optionally, the programmer can simply pass 0) passes a void pointer to allow the programemr
+		//to pass some of his own information
+		void F_Iterate_nodes(void (*function_pointer)(int,Node))
+		{
+			//ref_iterator provides information about which iteration the function is at
+		    int ref_iterator = 0;
+		    Node* dummy = LL_START_NODE;
+            while(dummy!=NULL)
+            {
+                //increment the iterator
+                ref_iterator++;
+				//calls the function with the iterator, and the node (by value, not as pointer)
+                function_pointer(ref_iterator,*dummy);
+                dummy = dummy->GetNext();
+            }
+		}
+		//the first and last nodes of the linked list
+        Node* LL_START_NODE;
+        Node* LL_END_NODE;
+    protected:
+    private:
+};
+
+#endif // LINKED_NODE_LIST_H
---
+++ Linked List/Original_code/Node/Node.h
@@ -0,0 +1,126 @@
+#ifndef NODE_H//is node already defined, avoids cyclic defines
+#define NODE_H
+
+#include "../Defines.h"
+class Node
+{
+    public:
+        Node()
+        {
+			//initialise the nodes information
+            LINKCOUNT = 0;
+            searchID = -1;
+            f_score = 0;
+            g_score = 0;
+            h_score = 0;
+            NEXT = NULL;
+            PREVIOUS = NULL;
+            parent = NULL;
+            s_udata = "";
+			//loop through the connections setting them to null
+            for(int i = 0;i<128;i++)
+            {
+                CONNECTIONS[i] = NULL;
+                FLAGS[i] = 0;
+            }
+        }
+        virtual ~Node()
+        {
+			//destructor
+            db<<"destructor called of node "<<s_udata<<"\n";
+        }
+        //Set the location of the node in the map
+        void SetCoord(float x_n, float y_n)
+        {x=x_n;y=y_n;}
+        //Get the x coordinate
+        float GetX(){return x;}
+        //Get the y coordinate
+        float GetY(){return y;}
+        //Get the next node in the linked list
+        Node* GetNext(){return NEXT;}
+        //Get the previous node in the linked list
+        Node* GetPrevious(){return PREVIOUS;}
+        //Sets the next node in the linked list
+        void SetNext(Node* dummy){NEXT = dummy;}
+        //Sets the previous node in the linked list;
+        void SetPrevious(Node* dummy){PREVIOUS = dummy;}
+        //Adds a new node to the network (actually just gives a pointer to a neighbour of this node
+        //128 is the maximum neighbours for the node, and should really be set as a constant in defines.h
+        int AddNode(Node* dummy, int flag)
+        {
+			//checks to ensure that not trying to add a null pointer
+            if(dummy!=NULL)
+            {
+				//checks that the linkcount is not exceeded
+                if(LINKCOUNT<128)
+                {
+					//checks to see if the node is alreay linked
+                    bool resident = false;
+                    for(int i = 0;i<LINKCOUNT;i++)
+                    {
+                        if(CONNECTIONS[i]->uid == dummy->uid)
+                        {
+                            db<<"the node is resident, not adding\n";
+                            resident = true;
+                        }
+                    }
+                    if(resident == true)
+                    {
+						//connection already there.
+                        return -3;
+                    }else{
+						//Add the connection before incrementing, to ensure correct position
+                        CONNECTIONS[LINKCOUNT] = dummy;
+                        FLAGS[LINKCOUNT] = flag;
+						//increment the linkcount
+                        LINKCOUNT++;
+						//return the resultant valency
+                        return LINKCOUNT;
+                    }
+                }else{
+					//too many connections
+                    return -2;
+                }
+           }else{
+				//null pointer
+				return -1;
+           }
+        }
+        //Gets the number of connected nodes (the valency)
+        int GetValency(){return LINKCOUNT;}
+        //Gets the adjacent node specified by i
+        Node* GetConnection(int i){return CONNECTIONS[i];}
+		//Gets the flags of the connection i
+        int GetFlag(int i){return FLAGS[i];};
+		//the unique id of the node
+        int uid;
+		//The userdata of the node, as a string
+        std::string s_udata;
+
+        //PATHFINDING INFORMATION
+		//Parent node in the path
+        Node* parent;
+		//The id of the most recent search performed on the node
+        int searchID;
+		//the f_score of the node, sum of g and h scores
+        double f_score;
+		//the g_score of the node, the distance from the start node (traced along the path)
+        double g_score;
+		//the heuristic distance to the final node (euclidian)
+        double h_score;
+    protected:
+    private:
+		//x/y location of the node
+        float x,y;
+		//number of current links
+        int LINKCOUNT;
+		//previous node in the linked list
+        Node* PREVIOUS;
+		//next node in the linked list
+        Node* NEXT;
+		//array of connections, limited to 128
+        Node* CONNECTIONS[128];
+		//flags for connections
+        int FLAGS[128];
+};
+#endif // NODE_H
